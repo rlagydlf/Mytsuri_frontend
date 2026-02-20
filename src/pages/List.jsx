f19b54d1374bb98d812eeb581938e42604ae9d57
(rlagydlf) <01021139593a@gmail.com>
2026-02-20 09:20:15 +0900

    그냥 리스트와 프로필 페이지

---
+++ src/pages/List.jsx
@@ -0,0 +1,13 @@
+import NavigationBar from '../components/NavigationBar'
+
+function Bookmark() {
+  return (
+    <div className="page-placeholder">
+      <h1>북마크</h1>
+      <p>북마크 페이지 (구현 예정)</p>
+      <NavigationBar />
+    </div>
+  )
+}
+
+export default Bookmark
---
+++ src/pages/Profile.jsx
@@ -0,0 +1,13 @@
+import NavigationBar from '../components/NavigationBar'
+
+function Profile() {
+  return (
+    <div className="page-placeholder">
+      <h1>마이</h1>
+      <p>마이 페이지 (구현 예정)</p>
+      <NavigationBar />
+    </div>
+  )
+}
+
+export default Profile
